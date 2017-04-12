7d4a60c14371d13927f655c06a5aca4709470f30
Ethan Bustad <ethan.bustad@liferay.com>
2017-04-12 16:11:23 -0700

    Fix typo in mysql statement

--- main.sh
+++ main.sh
@@ -43,7 +43,7 @@ fi
 
 echo "======================== Restoring data... ========================"
 
-mysql -u pr_tester -p testrayprtester < database/create.sql
+mysql -u pr_tester -ptestrayprtester < database/create.sql
 
 # -----------------------------------------------------------------------------
 # Sets up ant
