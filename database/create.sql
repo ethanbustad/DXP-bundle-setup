e7ca5e5afea8e1cdbd4acf3b602822b896a427de
Ethan Bustad <ethan.bustad@liferay.com>
2017-11-15 12:36:20 -0800

    Reset Sir Testalot's pw for easier testing

--- database/create.sql
+++ database/create.sql
@@ -3,3 +3,5 @@ create database lportal character set utf8;
 use lportal;
 
 source database/testray_ootb.sql
+
+update User_ set password_='testray', passwordEncrypted=false where emailAddress='sir.testalot@liferay.com';
