5629a58c03ca78ee7ddf97b5aad2c1680e7e8fb4
Ethan Bustad <ethan.bustad@liferay.com>
2017-11-15 16:49:54 -0800

    Switch to OSGi-ified testray

--- database/create.sql
+++ database/create.sql
@@ -5,3 +5,5 @@ use lportal;
 source database/testray_ootb.sql
 
 update User_ set password_='testray', passwordEncrypted=false where emailAddress='sir.testalot@liferay.com';
+
+update Release_ set servletContextName='com.liferay.osb.testray.service', schemaVersion='1.0.0' where servletContextName='osb-testray-portlet';
