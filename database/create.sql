49424215554bb188238c8a16ac166dd67cd19752
Ethan Bustad <ethan.bustad@liferay.com>
2017-11-16 13:42:56 -0800

    Use complete db dump, remove now unnecessary sql statements

--- database/create.sql
+++ database/create.sql
@@ -3,7 +3,3 @@ create database lportal character set utf8;
 use lportal;
 
 source database/testray_ootb.sql
-
-update User_ set password_='testray', passwordEncrypted=false where emailAddress='sir.testalot@liferay.com';
-
-update Release_ set servletContextName='com.liferay.osb.testray.service', schemaVersion='1.0.0' where servletContextName='osb-testray-portlet';
