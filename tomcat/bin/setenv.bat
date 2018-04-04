13a658736271f5db6a2d0c19a45b20218c61b32c
Ethan Bustad <ethan.bustad@liferay.com>
2018-04-04 15:44:16 -0700

    Add setenv.bat to match setenv.sh for windows

---
+++ tomcat/bin/setenv.bat
@@ -0,0 +1,11 @@
+set "CATALINA_OPTS=$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true  -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xms4096m -Xmx4096m -XX:MaxNewSize=2048m -XX:NewSize=2048m -XX:ReservedCodeCacheSize=128m"
+
+set "JAVA_OPTS=$JAVA_OPTS -XX:MaxTenuringThreshold=0 -XX:SurvivorRatio=65536 -XX:TargetSurvivorRatio=0"
+
+set "JAVA_OPTS=$JAVA_OPTS -server -d64"
+
+set "JAVA_OPTS=$JAVA_OPTS  -Xss768K -XX:+DisableExplicitGC -XX:+UseBiasedLocking"
+
+set "JAVA_OPTS=$JAVA_OPTS -XX:+CMSConcurrentMTEnabled -XX:+CMSCompactWhenClearAllSoftRefs -XX:CMSInitiatingOccupancyFraction=80 -XX:+CMSParallelRemarkEnabled -XX:+CMSScavengeBeforeRemark -XX:+UseConcMarkSweepGC -XX:+UseCompressedOops"
+
+set "JAVA_OPTS=$JAVA_OPTS -XX:ParallelCMSThreads=4 -XX:ParallelGCThreads=8 -XX:+UseParNewGC"
